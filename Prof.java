bf34be1567a15c864b4fdb77e38afca50b3e5dfc
vicenteavel <vicente_flip@hotmail.com>
2016-05-16 19:15:23 -0300

    Update Prof.java

--- Prof.java
+++ Prof.java
@@ -1,8 +1,11 @@
 public class Prof extends Funcionario{
 
-	protected String turmasDoProf[] = new String[10];
 	protected String matricula;
-
+	protected String turmasDoProf[] = new String[10];
+	
+	private int aux1 = 0; //varíavel para auxiliar o Set de Turmas
+	private String aux2 = ""; //varíavel para auxiliar o Get de Turmas
+	
 	// cargo vai ser iniciado como "professor"
 
 	public Prof(){
@@ -18,5 +21,20 @@ public class Prof extends Funcionario{
 	}
 	public String getMatricula(){
 		return this.matricula;
+	}
+	
+	public void setTurmasDoProf(String turma){
+		turmasDoProf[aux1] = turma;
+		aux1++;
+	}
+	public String getTurmasDoProf(){
+		for(int i = 0; i < turmasDoProf.length; i++){
+			aux2 += turmasdoProf[i] + "\n";
+		}
+		return aux2;
+	}
+	
+	public String toString(){
+		return super.toString() + "\nMatricula: " + matricula + "\nTurmas do Professor: " + getTurmasDoProf();
 	}
 }
