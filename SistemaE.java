9121c700d53cbe89a79cf87b77299f7ab92a528b
limafernando <luizfernandofpl@hotmail.com>
2016-05-18 13:01:57 -0300

    Update SistemaE.java

--- SistemaE.java
+++ SistemaE.java
@@ -51,7 +51,7 @@ public class SistemaE{
 			opcao = entrada.next();
 
 			switch (opcao){
-				case "1":
+				case "1": //Alunos
 					System.out.println("\n1 - Cadastrar aluno");
 					System.out.println("2 - Alterar dados do aluno"); //buscar pelo nome
 					System.out.println("3 - Remover aluno"); //buscar pelo nome
@@ -65,7 +65,7 @@ public class SistemaE{
 
 					switch (opcao){
 
-						case "1":
+						case "1": //Cadastrar aluno
 							entrada.nextLine(); //LIMPAR BUFFER DX SACANNER CHAMADO ENTRADA, MORAL, SE N DA RUIM
 							
 							System.out.print("\nInforme o nome: ");
@@ -74,52 +74,52 @@ public class SistemaE{
 							System.out.print("\nInforme o cpf: ");
 							alunos[contA].setCpf(entrada.next());
 
-							entrada.nextLine(); //LIMPAR BUFFER DX SACANNER CHAMADO ENTRADA, MORAL, SE N DA RUIM
+							entrada.nextLine(); //Limpando buffer
 
 							System.out.print("\nInforme o rg: ");
 							alunos[contA].setRg(entrada.next());
 
-							entrada.nextLine(); //LIMPAR BUFFER DX SACANNER CHAMADO ENTRADA, MORAL, SE N DA RUIM
+							entrada.nextLine(); //Limpando buffer
 							
 							System.out.print("\nInforme o matricula: ") ;
 							alunos[contA].setMatricula(entrada.next());
 
-							entrada.nextLine(); //LIMPAR BUFFER DX SACANNER CHAMADO ENTRADA, MORAL, SE N DA RUIM
+							entrada.nextLine(); //Limpando buffer
 							
 							/*System.out.print("\nInforme o telefone: ");
 							alunos[contA].setTelefone(entrada.next());
 
-							entrada.nextLine(); //LIMPAR BUFFER DX SACANNER CHAMADO ENTRADA, MORAL, SE N DA RUIM
+							entrada.nextLine(); //Limpando buffer
 							
 							System.out.print("\nInforme o email: ");
 							alunos[contA].setEmail(entrada.next());
 
-							entrada.nextLine(); //LIMPAR BUFFER DX SACANNER CHAMADO ENTRADA, MORAL, SE N DA RUIM
+							entrada.nextLine(); //Limpando buffer
 							
 							System.out.print("\nInforme o tipo sanguineo: ");
 							alunos[contA].setTipoSanguineo(entrada.next());
 
-							entrada.nextLine(); //LIMPAR BUFFER DX SACANNER CHAMADO ENTRADA, MORAL, SE N DA RUIM
+							entrada.nextLine(); //Limpando buffer
 							
 							System.out.print("\nInforme a turma: ");
 							alunos[contA].setTurmaDoAluno(entrada.next());
 
-							entrada.nextLine(); //LIMPAR BUFFER DX SACANNER CHAMADO ENTRADA, MORAL, SE N DA RUIM
+							entrada.nextLine(); //Limpando buffer
 
 							System.out.print("\nInforme o nome do responsavel: ");
 							alunos[contA].setNomeResponsavel(entrada.next());
 
-							entrada.nextLine(); //LIMPAR BUFFER DX SACANNER CHAMADO ENTRADA, MORAL, SE N DA RUIM
+							entrada.nextLine(); //Limpando buffer
 							
 							System.out.print("\nInforme o telefone do responsavel: ");
 							alunos[contA].setTelefoneResponsavel(entrada.next());
 
-							entrada.nextLine(); //LIMPAR BUFFER DX SACANNER CHAMADO ENTRADA, MORAL, SE N DA RUIM
+							entrada.nextLine(); //Limpando buffer
 							
 							System.out.print("\nInforme a mensalidade: ");
 							alunos[contA].setMensalidade(entrada.nextDouble());
 
-							entrada.nextLine(); //LIMPAR BUFFER DX SACANNER CHAMADO ENTRADA, MORAL, SE N DA RUIM
+							entrada.nextLine(); //Limpando buffer
 
 							//criar e utilizar um metodo de ordenação no array alunos*/
 
@@ -129,70 +129,73 @@ public class SistemaE{
 
 							break;
 
-						case "2":
-							entrada.nextLine(); //LIMPAR BUFFER DX SACANNER CHAMADO ENTRADA, MORAL, SE N DA RUIM
+						case "2": //Alterar dados de um aluno cadastrado
+							entrada.nextLine(); //Limpando buffer
 
 							System.out.print("\nInforme o nome do aluno: ");
 							buscarA = entrada.nextLine();
 
+							System.out.print("\nInforme a matricula do aluno: ");
+							matriculaB = entrada.nextLine();
+
 							aluno = false;
+							//Busca
 							for(int i = 0; i < alunos.length; i++){
-								if(buscarA.equals(alunos[i].getNome())){
+								if(buscarA.equals(alunos[i].getNome()) && matriculaB.equals(alunos[i].getMatricula())){
 
 									aluno = true;
-							
 									System.out.print("\nInforme o nome: ");
 									alunos[i].setNome(entrada.nextLine());
 
 									System.out.print("\nInforme o cpf: ");
 									alunos[i].setCpf(entrada.next());
 
-									entrada.nextLine(); //LIMPAR BUFFER DX SACANNER CHAMADO ENTRADA, MORAL, SE N DA RUIM
+									entrada.nextLine(); //Limpando buffer
 
 									System.out.print("\nInforme o rg: ");
 									alunos[i].setRg(entrada.next());
 
-									entrada.nextLine(); //LIMPAR BUFFER DX SACANNER CHAMADO ENTRADA, MORAL, SE N DA RUIM
+									entrada.nextLine(); //Limpando buffer
 									
 									System.out.print("\nInforme o matricula: ") ;
 									alunos[i].setMatricula(entrada.next());
 
-									entrada.nextLine(); //LIMPAR BUFFER DX SACANNER CHAMADO ENTRADA, MORAL, SE N DA RUIM
+									entrada.nextLine(); //Limpando buffer
 									
 									/*System.out.print("\nInforme o telefone: ");
 									alunos[i].setTelefone(entrada.next());
 
-									entrada.nextLine(); //LIMPAR BUFFER DX SACANNER CHAMADO ENTRADA, MORAL, SE N DA RUIM
+									entrada.nextLine(); //Limpando buffer
 									
 									System.out.print("\nInforme o email: ");
 									alunos[i].setEmail(entrada.next());
 
-									entrada.nextLine(); //LIMPAR BUFFER DX SACANNER CHAMADO ENTRADA, MORAL, SE N DA RUIM
+									entrada.nextLine(); //Limpando buffer
 									
 									System.out.print("\nInforme o tipo sanguineo: ");
 									alunos[i].setTipoSanguineo(entrada.next());
 
-									entrada.nextLine(); //LIMPAR BUFFER DX SACANNER CHAMADO ENTRADA, MORAL, SE N DA RUIM
+									entrada.nextLine(); //Limpando buffer
 									
 									System.out.print("\nInforme a turma: ");
 									alunos[i].setTurmaDoAluno(entrada.next());
 
-									entrada.nextLine(); //LIMPAR BUFFER DX SACANNER CHAMADO ENTRADA, MORAL, SE N DA RUIM
+									entrada.nextLine(); //Limpando buffer
 
 									System.out.print("\nInforme o nome do responsavel: ");
 									alunos[i].setNomeResponsavel(entrada.next());
 
-									entrada.nextLine(); //LIMPAR BUFFER DX SACANNER CHAMADO ENTRADA, MORAL, SE N DA RUIM
+									entrada.nextLine(); //Limpando buffer
 									
 									System.out.print("\nInforme o telefone do responsavel: ");
 									alunos[i].setTelefoneResponsavel(entrada.next());
 
-									entrada.nextLine(); //LIMPAR BUFFER DX SACANNER CHAMADO ENTRADA, MORAL, SE N DA RUIM
+									entrada.nextLine(); Limpando buffer
 									
 									System.out.print("\nInforme a mensalidade: ");
 									alunos[i].setMensalidade(entrada.nextDouble());
 
-									entrada.nextLine(); //LIMPAR BUFFER DX SACANNER CHAMADO ENTRADA, MORAL, SE N DA RUIM*/
+									entrada.nextLine(); //Limpando buffer*/
 
 									System.out.println("\nDados alterados com sucesso!");
 								}
@@ -204,18 +207,31 @@ public class SistemaE{
 
 							break;
 
-						case "3":
-							entrada.nextLine(); //LIMPAR BUFFER DX SACANNER CHAMADO ENTRADA, MORAL, SE N DA RUIM
+						case "3": //Remover um aluno cadastrado
+							entrada.nextLine(); //Limpando buffer
 
 							System.out.print("\nInforme o nome do aluno: ");
 							buscarA = entrada.nextLine();
 
+							matriculaB = "";
+							System.out.print("\nInforme a matricula do aluno: ");
+							matriculaB = entrada.nextLine();
+
+							//utilizar o metodo de busca
+							//remover o elemento desejado do array alunos
+							//reordenar
+
 							aluno = false;
+							//Busca
 							for(int i = 0; i < alunos.length; i++){
 								if(buscarA.equals(alunos[i].getNome()) && matriculaB.equals(alunos[i].getMatricula())){
 									aluno = true;
 									
-									for(int j = 0; j < alunos.length; j++){
+									//Caso o aluno desejado seja encontrado, cada elemento do array alunos, a partir do elemento
+									//que se encontra do aluno desejado, vai receber o conteúdo do elemento a frente, exceto se
+									//o elemento for o último do array, neste caso, ele vai ser instanciado
+
+									for(int j = i; j < alunos.length; j++){
 										
 										if(j == (alunos.length - 1)){
 											alunos[j] = new Aluno();
@@ -228,7 +244,7 @@ public class SistemaE{
 
 									System.out.println("\nAluno removido com sucesso!");
 
-									--contA; //contador de alunos vai ser decrementado
+									--contA; //Contador de alunos vai ser decrementado
 								}
 							}
 
@@ -238,17 +254,23 @@ public class SistemaE{
 
 							break;
 
-						case "4":
-							entrada.nextLine(); //LIMPAR BUFFER DX SACANNER CHAMADO ENTRADA, MORAL, SE N DA RUIM
+						case "4": //Consultar dados de um aluno
+							entrada.nextLine(); //Limpando buffer
 							
 							System.out.print("\nInforme o nome do aluno: ");
 							buscarA = entrada.nextLine();
 
+							System.out.print("\nInforme a matricula do aluno: ");
+							matriculaB = entrada.nextLine();
+
 							aluno = false;
+							//Busca
 							for(int i = 0; i < alunos.length; i++){
-								if(buscarA.equals(alunos[i].getNome())){
+								if(buscarA.equals(alunos[i].getNome()) && matriculaB.equals(alunos[i].getMatricula())){
+									
 									aluno = true;
-									System.out.print(alunos[i]);
+									System.out.print(alunos[i]); //Printa todas as informações do aluno em tela
+
 								}
 							}
 
@@ -259,17 +281,18 @@ public class SistemaE{
 							break;
 
 						case "5":
-							entrada.nextLine(); //LIMPAR BUFFER DX SACANNER CHAMADO ENTRADA, MORAL, SE N DA RUIM
+							entrada.nextLine(); //Limpando buffer
 
 							break;
 							
-						case "6":
+						case "6": //Gerar um documento de comprovação de matricula
 							break;
 
-						case "0":
+						case "0": //Voltar
 							break;
 
 						default:
+							System.out.println("\nOpcao invalida!");
 							break;
 					}
 
