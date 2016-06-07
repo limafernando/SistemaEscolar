8b20ecc74bf5743bbb208c53046cae6f21438b3f
limafernando <luizfernandofpl@hotmail.com>
2016-06-07 17:34:24 -0300

    Update Funcionario.java

--- Funcionario.java
+++ Funcionario.java
@@ -1,3 +1,6 @@
+package sistemaEscolar;
+import javax.swing.JOptionPane;
+
 public class Funcionario extends Pessoa{
 	
 	protected String cargo;
@@ -34,4 +37,82 @@ public class Funcionario extends Pessoa{
 	public String toString(){
 		return super.toString() + "\nCargo: " + cargo + "\nSalário: " + salario;
 	}
+        
+        public void cadastrarFuncionario(Funcionario funcionario){
+            //Cadastrar dados de um funcionario
+            
+            funcionario.setNome(JOptionPane.showInputDialog(null, "Informe o nome: "));
+
+            funcionario.setCpf(JOptionPane.showInputDialog(null, "Informe o CPF: "));
+           
+            /*
+            funcionario.setRg(JOptionPane.showInputDialog(null, "Informe o RG: "));
+            
+            funcionario.setMatricula(JOptionPane.showInputDialog(null, "Informe a matricula: "));
+
+            funcionario.setTelefone(JOptionPane.showInputDialog(null, "Informe o telefone: "));
+            
+            funcionario.setEmail(JOptionPane.showInputDialog(null, "Informe o email: "));
+           
+            funcionario.setTipoSanguineo(JOptionPane.showInputDialog(null, "Informe o tipo sanguineo: "));
+            
+            funcionario.setCargo(JOptionPane.showInputDialog(null, "Informe o cargo: "));
+            
+            funcionario.setSalario(Double.parseDouble(JOptionPane.showInputDialog(null, "Informe o salario: ")));
+            
+            */
+							
+            JOptionPane.showMessageDialog(null, "Funcionario cadastrado com sucesso!");
+        }
+        
+        public void alterarDadosFuncionario(Funcionario funcionario){
+            //Alterar dados de um funcionario
+            
+            funcionario.setNome(JOptionPane.showInputDialog(null, "Informe o nome: "));
+
+            funcionario.setCpf(JOptionPane.showInputDialog(null, "Informe o CPF: "));
+           
+            /*
+            funcionario.setRg(JOptionPane.showInputDialog(null, "Informe o RG: "));
+            
+            funcionario.setMatricula(JOptionPane.showInputDialog(null, "Informe a matricula: "));
+
+            funcionario.setTelefone(JOptionPane.showInputDialog(null, "Informe o telefone: "));
+            
+            funcionario.setEmail(JOptionPane.showInputDialog(null, "Informe o email: "));
+           
+            funcionario.setTipoSanguineo(JOptionPane.showInputDialog(null, "Informe o tipo sanguineo: "));
+            
+            funcionario.setCargo(JOptionPane.showInputDialog(null, "Informe o cargo: "));
+            
+            funcionario.setSalario(Double.parseDouble(JOptionPane.showInputDialog(null, "Informe o salario: ")));
+            
+            */
+							
+            JOptionPane.showMessageDialog(null, "Dados alterados com sucesso!");
+        }
+        
+        public void removerFuncionario(Funcionario funcionarios[], int posicao){
+            //Remover um funcionario cadastrado
+							
+            for(int j = posicao; j < funcionarios.length; j++){								
+									
+                if(j == (funcionarios.length - 1)){					
+                    funcionarios[j] = new Funcionario();
+                }
+		
+                else{
+                    funcionarios[j] = funcionarios[j+1];
+                }
+            }
+            
+            JOptionPane.showMessageDialog(null, "Funcionario removido com sucesso!");
+        }
+        
+        public void consultarFuncionario(Funcionario funcionario){
+            //Consultar dados de um funcionario
+            
+            JOptionPane.showMessageDialog(null, funcionario);
+        }
+        
 }
