10f744becb7e37d9c2ed9313d86e62fb62a6cd0e
c3z410 <caioedu1@hotmail.com>
2016-06-08 23:48:11 -0300

    Update Turma.java

--- Turma.java
+++ Turma.java
@@ -1,10 +1,10 @@
 package sistemaEscolar;
 
 import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
 
 
 public class Turma{
-
 	
 	protected String id;
 	protected Aluno alunos[] = new Aluno[50];
@@ -59,8 +59,9 @@ public class Turma{
     	
     	String buscarT = JOptionPane.showInputDialog(null, "Informe o id da turma: ");
     	boolean verifica = false;
+    	if(!buscarT.isEmpty()){
 		for(int j = 0; j < turmas.length; j++){
-			if(buscarT.equals(turmas[j].getId())){
+			if(buscarT.equals(turmas[j].getId())&& (!buscarT.isEmpty())){
 				verifica=true;
 				JOptionPane.showMessageDialog(null, "Turma existente");
 			}
@@ -68,6 +69,9 @@ public class Turma{
 		if(!verifica){turmas[cont].setId(buscarT);
 		JOptionPane.showMessageDialog(null, "Turma cadastrado com sucesso!");}
 	}
+    	else{JOptionPane.showMessageDialog(null, "Turma invalida");}
+	}	
+    
 	public void registrarAlunos(Aluno alunos[],Turma turmas[]){
     	String buscarT = JOptionPane.showInputDialog(null, "Informe o id da turma: ");
 		boolean verifica = false;
@@ -76,13 +80,18 @@ public class Turma{
 				verifica=true;
 				for(int i = 0; i < alunos.length; i++){
 					if(alunos[i].getTurmadoAluno().equals(turmas[j].getId())){
-						turmas[j].setAlunos(alunos[i]);
+						boolean verifica2 = false;
+						for(int k = 0; k < turmas[j].alunos.length; k++){
+							if(alunos[i].getMatricula().equals(turmas[j].alunos[k].getMatricula())){
+								verifica2 = true;
+							}
+						}
+						if(!verifica2){turmas[j].setAlunos(alunos[i]);}
 					}   
 				}
         
 				for(int i = 1; i < turmas[j].alunos.length;i++){
 					Aluno chave = turmas[j].alunos[i];
-					//System.out.println(chave);
 					if (chave.getNome() == null || chave.getNome().isEmpty()) {
 						break;
 					}
@@ -99,9 +108,10 @@ public class Turma{
 		}
 		if(!verifica){JOptionPane.showMessageDialog(null, "Turma inexistente");}
 		else{JOptionPane.showMessageDialog(null, "Alunos cadastrados com sucesso!");}
-	}
-        
-        
+		}
+	
+      
+	
         
         public void registrarProfessor(Prof professores[], Turma turmas[]){
         	String buscarT = JOptionPane.showInputDialog(null, "Informe o id da turma: ");
@@ -111,8 +121,14 @@ public class Turma{
     				verifica=true;
     				for(int i = 0; i < professores.length; i++){
     					for(int k = 0; k < professores[i].turmasDoProf.length; k++){
-    						if(professores[i].getTurmasDoProf2(k).equals(turmas[j].getId())){
-    							turmas[j].setProfessores(professores[i]);
+    						if(professores[i].getTurmasDoProf(k).equals(turmas[j].getId())){
+    							boolean verifica2 = false;
+    							for(int h = 0; h < turmas[j].professores.length; h++){
+    								if(professores[i].getNome().equals(turmas[j].professores[h].getNome())){
+    									verifica2 = true;
+    								}
+    							}
+    							if(!verifica2){turmas[j].setProfessores(professores[i]);}
     						}
     					}
     				}
@@ -127,31 +143,56 @@ public class Turma{
         public void consultarTurma(Turma turmas[]){
         	String buscarT = JOptionPane.showInputDialog(null, "Informe o id da turma: ");
     		boolean verifica = false;
+    		String a = "Professores\n";
     		for(int j = 0; j < turmas.length; j++){
-    			if(buscarT.equals(turmas[j].getId())){
+    			if(buscarT.equals(turmas[j].getId())&& (!buscarT.isEmpty())){
     				verifica=true;
     				for(int i = 0; i < turmas[j].professores.length;i++){
     					if (turmas[j].professores[i].getNome() == null || 
     						turmas[j].professores[i].getNome().isEmpty()) {
     						break;
 				}
-				System.out.println(turmas[j].professores[i].getNome()/*getMateria?*/);
+    					a+="\n"+turmas[j].professores[i].getNome();
 			}
-
+    		
+    		a+="\n\nAlunos\nNumero\tNome\t\tMatricula";
 			for(int i = 0; i < turmas[j].alunos.length;i++){
 				if (turmas[j].alunos[i].getNome() == null || 
 					turmas[j].alunos[i].getNome().isEmpty()) {
 					break;
 				}
-				JOptionPane.showMessageDialog(null,"\n"+i+1+" "+" "+turmas[j].alunos[i].getNome()+" "+turmas[j].alunos[i].getMatricula());
+				a+="\n"+(i+1)+"\t"+turmas[j].alunos[i].getNome()+"\t"+turmas[j].alunos[i].getMatricula();
 			}
+			JOptionPane.showMessageDialog(null, new JTextArea(a));
     		}
     		}
     		if(!verifica){JOptionPane.showMessageDialog(null, "Turma inexistente");}
 
         }
-        
-        /*metodo remover turma utilizara o mesmo esqueme de aluno e professor,
-          mas tera de modificar a string turma de professor e aluno*/
+        public void alterarTurma(Turma turmas[],Prof professores[],Aluno alunos[]){
+        	String buscarT = JOptionPane.showInputDialog(null, "Informe o id da turma: ");
+        	String mudarT = JOptionPane.showInputDialog(null, "Informe o novo id da turma: ");
+    		boolean verifica = false;	
+    		for(int j = 0; j < turmas.length; j++){
+    			if(buscarT.equals(turmas[j].getId())){
+    				verifica=true;
+    				for(int i = 0; i < alunos.length; i++){
+    					if(alunos[i].getTurmadoAluno().equals(turmas[j].getId())){
+    						alunos[i].setTurmaDoAluno(mudarT);
+    					}
+    				}
+    				for(int i = 0; i < professores.length; i++){
+    					for(int k = 0; k < professores[i].turmasDoProf.length; k++){
+    						if(professores[i].getTurmasDoProf(k).equals(turmas[j].getId())){
+    							professores[i].setTurmasDoProf(mudarT);
+    						}
+    					}
+    				}	
+    				turmas[j].setId(mudarT);
+    			}	
+    		}
+    		if(!verifica){JOptionPane.showMessageDialog(null, "Turma inexistente");}
+    		else{JOptionPane.showMessageDialog(null, "Turma alterada com sucesso!");}
+        }
+    
 }
-
